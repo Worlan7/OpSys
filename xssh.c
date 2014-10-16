7de8c98318ad6ba01aa67257aa2ebcc8288b5b7c
Elom Worlanyo <elom.worlanyo@answers.com>
2014-10-16 16:10:12 -0500

    Parsing command line arguments

--- xssh.c
+++ xssh.c
@@ -21,12 +21,83 @@
 #define MAXLINE    1024   /* max line size */
 #define MAXARGS     128   /* max args on a command line */
 
-/* Bits in third argument to 'waitpid'. */
-#define WNOHANG     1   /* Don't block waiting. */
-#define WUNTRACED   2   /* Report status of stopped children. */
-
 /* Global variables */
 extern char **environ;      /* defined in libc */
 char prompt[] = "xssh>> ";    /* command line prompt */
 int verbose = 0;            /* if true, print additional output */
+int disp_cmdl = 0;		/* if true, display command line after var sub */
+char *file_args;	/* Any optional file arguments go here*/ 
 char sbuf[MAXLINE];         /* for composing sprintf messages */
+/* End global variables */
+
+
+void usage(void);
+
+
+/*
+ * main - The shell's main routine 
+ */
+int main(int argc, char **argv) 
+{
+    char c;
+    char cmdline[MAXLINE];
+
+    /* Redirect stderr to stdout (so that driver will get all output
+     * on the pipe connected to stdout) */
+    dup2(1, 2);
+
+    /* Parse the command line */
+    while ((c = getopt(argc, argv, "xd:f:")) != EOF) {
+        switch (c) {
+        case 'x':           /* print the command line after variable */ 
+            printf("x is in \n");
+            disp_cmdl = 1;		/* substitution is displayed before */
+        break;		       	/* the command is evaluated. w/e that means*/
+      
+        case 'd':             /* emit additional diagnostic info */
+            printf("%s", optarg);
+            printf(" d is in \n");
+        	if(isdigit(*optarg)){
+            	verbose = atoi(optarg);
+            }else{
+            	usage();
+            }
+        break;
+       
+        case 'f':             /* change input from stdin to file */
+          	file_args = optarg ;  /* optional args assigned to shell vars */
+         	printf("%s", file_args);
+           	printf(" f is in \n");
+          	exit;
+        break;
+    default:
+            usage();
+    }
+    }
+
+    /* Execute the shell's read/eval loop */
+    while (1) {
+
+    } 
+
+    exit(0); /* control never reaches here */
+}
+
+
+
+/***********************
+ * Other helper routines
+ ***********************/
+
+/*
+ * usage - print a help message
+ */
+void usage(void) 
+{
+    printf("Usage: shell [-hvp]\n");
+    printf("   -h   print this message\n");
+    printf("   -v   print additional diagnostic information\n");
+    printf("   -p   do not emit a command prompt\n");
+    exit(1);
+}
+
